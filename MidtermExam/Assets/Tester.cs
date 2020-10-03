d45a473c642cc49bc1bba3ddb815e48bf2e26c74
DESKTOP-325D3AA\jade <chetsada_changphanit@cmu.ac.th>
2020-10-03 09:56:37 +0700

    First push SceneManager

---
+++ MidtermExam/Assets/Tester.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SingletonGameApplicationManager.Instance.Level = 99;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
